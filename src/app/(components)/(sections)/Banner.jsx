3ee549d1ddd948b69b8f46bcea6d8bd2388d5284
yhsunny176 <yeasinulhaq763@gmail.com>
2025-08-22 21:58:41 +0600

    feat: Banner Section with CTA Button and 3d Image has been done

--- src/app/(components)/(sections)/Banner.jsx
+++ src/app/(components)/(sections)/Banner.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Banner() {
     return (
         <section id="banner-section">
-            <div className="min-h-screen bg-gray-100">This is Banner</div>
+            <div className="max-w-9/12 mt-12 py-16 px-16 rounded-2xl mx-auto h-[680px] bg-gradient-to-l from-cyan-500 to-blue-500">
+                <div className="flex h-full items-center justify-between">
+                    {/* Left Section */}
+                    <div className="flex-1 pr-8 flex flex-col justify-center">
+                        
+                        <h1 className="text-6xl font-bold text-white mb-6 font-outfit">Medoza: Trusted Care, Delivered</h1>
+                        <p className="text-lg text-white mb-4 font-montserrat">
+                            Empowering you to access authentic medicines, expert guidance, and seamless delivery—because
+                            every dose matters.
+                        </p>
+
+                        <Link href={"/medicines"} className="text-button-text-secondary font-montserrat bg-cyan-primary hover:scale-105 transition-all duration-600 ease-in-out cursor-pointer py-4 px-6 font-semibold rounded-xl max-w-max">Buy Medicine</Link>
+                        
+                    </div>
+                    {/* Right Section */}
+                    <div className="flex-1 flex justify-center items-center">
+                        <img src="/3d-background-banner.webp" alt="Medicine Banner" className="w-[600px] h-auto" />
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
